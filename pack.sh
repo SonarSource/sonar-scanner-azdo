c9bc917cd9b9592d36b7d09bdd3d8e47840e98f3
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2017-12-05 16:34:48 +0100

    VSTS-117 Remove old packaging scripts

--- pack.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-// TODO Copy the correct logo from tasks/common/icon.png to each task folder
-// TODO Copy common/v3/SonarQubeHelper.ps1 to each v3 task folder
-// TODO unzip the scanner-cli distribution in the scanner-cli task folder
-
-tfx extension create
