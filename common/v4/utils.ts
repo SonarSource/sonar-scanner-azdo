e8d18352b9151c69715b2592a5a48675c6798a19
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2017-12-11 11:46:30 +0100

    VSTS-117 Add common ts files for v4 tasks

---
+++ common/v4/utils.ts
@@ -0,0 +1,19 @@
+export const PROP_NAMES = {
+  HOST_URL: 'sonar.host.url',
+  LOGIN: 'sonar.login',
+  PASSSWORD: 'sonar.password',
+  ORG: 'sonar.organization',
+  PROJECTKEY: 'sonar.projectKey',
+  PROJECTNAME: 'sonar.projectName',
+  PROJECTVERSION: 'sonar.projectVersion'
+};
+
+export function toCleanJSON(props: { [key: string]: string }) {
+  return JSON.stringify(props, Object.keys(props).filter(key => props[key] != null));
+}
+
+export function setIfNotEmpty(props: { [key: string]: string }, key: string, value: string) {
+  if (value) {
+    props[key] = value;
+  }
+}
