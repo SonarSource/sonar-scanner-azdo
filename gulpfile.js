e027a60dfa1988bb9931b854d345b6f5373bcacf
Julien HENRY <henryju@yahoo.fr>
2017-12-13 20:36:04 +0100

    Enable deployment on repox

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const download = require('gulp-download');
 const gulp = require('gulp');
 const gulpif = require('gulp-if');
 const gulpSequence = require('gulp-sequence');
+const artifactoryUpload = require('gulp-artifactory-upload');
 const gutil = require('gulp-util');
 const jeditor = require('gulp-json-editor');
 const rename = require('gulp-rename');
@@ -12,6 +13,10 @@ const { argv } = require('yargs');
 const es = require('event-stream');
 const fs = require('fs-extra');
 const semver = require('semver');
+const crypto = require('crypto');
+const through = require('through2');
+const request = require('request');
+const dateformat = require('dateformat');
 const extensionTest = require('./vss-extension.test.json');
 const { bundleTsTask, pathAllFiles, npmInstallTask, tfxCommand } = require('./package-utils');
 
@@ -109,9 +114,13 @@ gulp.task('tasks:new:bundle', ['tasks:new:npminstall'], () =>
   )
 );
 
+function getPackageJSON() {
+  return JSON.parse(fs.readFileSync('package.json'));
+}
+
 function fullVersion() {
   var buildNumber = process.env.TRAVIS_BUILD_NUMBER;
-  var packageJSON = JSON.parse(fs.readFileSync('package.json'));
+  var packageJSON = getPackageJSON();
   var version = packageJSON.version;
   if (version.endsWith('-SNAPSHOT') && buildNumber) {
     return version.replace('-SNAPSHOT', '.' + buildNumber);
@@ -120,7 +129,7 @@ function fullVersion() {
 }
 
 function semVer() {
-  var packageJSON = JSON.parse(fs.readFileSync('package.json'));
+  var packageJSON = getPackageJSON();
   return packageJSON.version.replace('-SNAPSHOT', '');
 }
 
@@ -202,6 +211,140 @@ gulp.task('tfx', () => tfxCommand(paths.build.extension));
 gulp.task('tfx:test', () =>
   tfxCommand(paths.build.extension, `--publisher ` + (argv.publisher || 'foo'))
 );
+
+var hashes = {
+  sha1: '',
+  md5: ''
+};
+
+gulp.task('compute-hashes', ['build'], function() {
+  return gulp.src(path.join(paths.build.root, '*.vsix')).pipe(hashsum());
+});
+
+gulp.task('deploy-vsix', ['build', 'compute-hashes'], function() {
+  if (process.env.TRAVIS_BRANCH != 'master' && process.env.TRAVIS_PULL_REQUEST == 'false') {
+    gutil.log('Not on master nor PR, skip deploy-buildinfo');
+    return;
+  }
+  var packageJSON = getPackageJSON();
+  var version = fullVersion();
+  var name = packageJSON.name;
+  var buildNumber = process.env.TRAVIS_BUILD_NUMBER;
+  return gulp
+    .src('*.vsix')
+    .pipe(
+      artifactoryUpload({
+        url:
+          process.env.ARTIFACTORY_URL +
+          '/' +
+          process.env.ARTIFACTORY_DEPLOY_REPO +
+          '/org/sonarsource/scanner/vsts/' +
+          name +
+          '/' +
+          version,
+        username: process.env.ARTIFACTORY_DEPLOY_USERNAME,
+        password: process.env.ARTIFACTORY_DEPLOY_PASSWORD,
+        properties: {
+          'vcs.revision': process.env.TRAVIS_COMMIT,
+          'vcs.branch': process.env.TRAVIS_BRANCH,
+          'build.name': name,
+          'build.number': process.env.TRAVIS_BUILD_NUMBER
+        },
+        request: {
+          headers: {
+            'X-Checksum-MD5': hashes.md5,
+            'X-Checksum-Sha1': hashes.sha1
+          }
+        }
+      })
+    )
+    .on('error', gutil.log);
+});
+
+gulp.task('deploy-buildinfo', ['compute-hashes'], function() {
+  if (process.env.TRAVIS_BRANCH != 'master' && process.env.TRAVIS_PULL_REQUEST == 'false') {
+    gutil.log('Not on master nor PR, skip deploy-buildinfo');
+    return;
+  }
+  var packageJSON = getPackageJSON();
+  var version = fullVersion();
+  var name = packageJSON.name;
+  var buildNumber = process.env.TRAVIS_BUILD_NUMBER;
+  return request
+    .put(
+      {
+        url: process.env.ARTIFACTORY_URL + '/api/build',
+        json: buildInfo(name, version, buildNumber, hashes)
+      },
+      function(error, response, body) {
+        if (error) {
+          gutil.log('error:', error);
+        }
+      }
+    )
+    .auth(process.env.ARTIFACTORY_DEPLOY_USERNAME, process.env.ARTIFACTORY_DEPLOY_PASSWORD, true);
+});
+
+gulp.task('deploy', ['deploy-buildinfo', 'deploy-vsix'], function() {});
+
+function buildInfo(name, version, buildNumber, hashes) {
+  return {
+    version: '1.0.1',
+    name: name,
+    number: buildNumber,
+    started: dateformat(new Date(), "yyyy-mm-dd'T'HH:MM:ss.lo"),
+    url: process.env.CI_BUILD_URL,
+    vcsRevision: process.env.TRAVIS_COMMIT,
+    vcsUrl: 'https://github.com/' + process.env.TRAVIS_REPO_SLUG + '.git',
+    modules: [
+      {
+        id: 'org.sonarsource.scanner.vsts:' + name + ':' + version,
+        properties: {
+          artifactsToPublish: 'org.sonarsource.scanner.vsts:' + name + ':vsix'
+        },
+        artifacts: [
+          {
+            type: 'vsix',
+            sha1: hashes.sha1,
+            md5: hashes.md5,
+            name: name + '-' + version + '.vsix'
+          }
+        ]
+      }
+    ],
+    properties: {
+      'buildInfo.env.PROJECT_VERSION': version,
+      'buildInfo.env.ARTIFACTORY_DEPLOY_REPO': 'sonarsource-public-qa',
+      'buildInfo.env.TRAVIS_COMMIT': process.env.TRAVIS_COMMIT
+    }
+  };
+}
+
+function hashsum() {
+  function processFile(file, encoding, callback) {
+    if (file.isNull()) {
+      return;
+    }
+    if (file.isStream()) {
+      gutil.log('Streams not supported');
+      return;
+    }
+    for (var algo in hashes) {
+      if (hashes.hasOwnProperty(algo)) {
+        hashes[algo] = crypto
+          .createHash(algo)
+          .update(file.contents, 'binary')
+          .digest('hex');
+        gutil.log('Computed ' + algo + ': ' + hashes[algo]);
+      }
+    }
+
+    this.push(file);
+    callback();
+  }
+
+  return through.obj(processFile);
+}
 
 gulp.task('copy', [
   'extension:copy',
