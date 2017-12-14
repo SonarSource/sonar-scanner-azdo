4ede734a9e044da1faf621e574c31a8f940dc0da
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2017-12-14 11:58:44 +0100

    VSTS-126 Create TaskReport class to find and parse a report-task file

---
+++ common/ts/TaskReport.ts
@@ -0,0 +1,122 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as tl from 'vsts-task-lib/task';
+
+export const REPORT_TASK_NAME = 'report-task.txt';
+
+interface ITaskReport {
+  projectKey: string;
+  ceTaskId: string;
+  serverUrl: string;
+  dashboardUrl?: string;
+  ceTaskUrl?: string;
+}
+
+export default class TaskReport {
+  constructor(private report: ITaskReport) {
+    for (const field of ['projectKey', 'ceTaskId', 'serverUrl']) {
+      if (!report[field]) {
+        throw TaskReport.throwMissingField(field);
+      }
+    }
+  }
+
+  public get projectKey() {
+    return this.report.projectKey;
+  }
+
+  public get ceTaskId() {
+    return this.report.ceTaskId;
+  }
+
+  public get serverUrl() {
+    return this.report.serverUrl;
+  }
+
+  public get dashboardUrl() {
+    return this.report.dashboardUrl;
+  }
+
+  public static findTaskFileReport(): string | undefined {
+    const taskReportGlob = path.join('**', REPORT_TASK_NAME);
+    const taskReportGlobResult = tl.findMatch(
+      tl.getVariable('System.DefaultWorkingDirectory'),
+      taskReportGlob
+    );
+    tl.debug(`[SQ] Searching for ${taskReportGlob} - found ${taskReportGlobResult.length} file(s)`);
+
+    if (taskReportGlobResult.length > 1) {
+      tl.warning(
+        `[SQ] Multiple '${REPORT_TASK_NAME}' files found. Choosing the first one. ` +
+          `The build summary may not be accurate. ` +
+          `Possible cause: multiple SonarQube analysis during the same build, which is not supported.`
+      );
+    }
+
+    return taskReportGlobResult[0];
+  }
+
+  public static createTaskReportFromFile(
+    filePath = TaskReport.findTaskFileReport()
+  ): Promise<TaskReport> {
+    return new Promise((resolve, reject) => {
+      if (!filePath) {
+        reject(
+          TaskReport.throwInvalidReport(
+            `[SQ] Could not find '${REPORT_TASK_NAME}'.` +
+              ` Possible cause: the SonarQube analysis did not complete successfully.`
+          )
+        );
+      }
+      tl.debug(`[SQ] Read Task report file: ${filePath}`);
+      fs.access(filePath, fs.constants.R_OK, err => {
+        if (err) {
+          return reject(
+            TaskReport.throwInvalidReport(`[SQ] Task report not found at: ${filePath}`)
+          );
+        }
+        fs.readFile(filePath, 'utf-8', (err, fileContent) => {
+          tl.debug(`[SQ] Parse Task report file: ${fileContent}`);
+          if (err || !fileContent || fileContent.length <= 0) {
+            return reject(TaskReport.throwInvalidReport(`[SQ] Error reading file: ${fileContent}`));
+          }
+          try {
+            const settings = TaskReport.createTaskReportFromString(fileContent);
+            const taskReport = new TaskReport({
+              projectKey: settings.get('projectKey'),
+              serverUrl: settings.get('serverUrl'),
+              dashboardUrl: settings.get('dashboardUrl'),
+              ceTaskId: settings.get('ceTaskId'),
+              ceTaskUrl: settings.get('ceTaskUrl')
+            });
+            return resolve(taskReport);
+          } catch (err) {
+            tl.debug(`[SQ] Parse Task report error: ${err.message}`);
+            return reject(err);
+          }
+        });
+      });
+    });
+  }
+
+  private static createTaskReportFromString(fileContent: string): Map<string, string> {
+    const lines: string[] = fileContent.replace(/\r\n/g, '\n').split('\n'); // proofs against xplat line-ending issues
+    const settings = new Map<string, string>();
+    lines.forEach((line: string) => {
+      const splitLine = line.split('=');
+      if (splitLine.length > 1) {
+        settings.set(splitLine[0], splitLine.slice(1, splitLine.length).join());
+      }
+    });
+    return settings;
+  }
+
+  private static throwMissingField(field: string): Error {
+    return new Error(`Failed to create TaskReport object. Missing field: ${field}`);
+  }
+
+  private static throwInvalidReport(debugMsg: string): Error {
+    tl.debug(debugMsg);
+    return new Error('Invalid or missing task report. Check SonarQube finished successfully.');
+  }
+}
