2bf0ef5b39c99e4cfbd1a51dfbb94f8dbb468d29
Julien HENRY <julien.henry@sonarsource.com>
2018-02-23 15:17:48 +0100

    VSTS-136 Automatically pass properties needed for branch/PR analysis

--- common/ts/prepare-task.ts
+++ common/ts/prepare-task.ts
@@ -1,13 +1,22 @@
 import * as tl from 'vsts-task-lib/task';
-import Endpoint from './sonarqube/Endpoint';
+import * as vm from 'vso-node-api';
+import Endpoint, { EndpointType } from './sonarqube/Endpoint';
 import Scanner, { ScannerMode } from './sonarqube/Scanner';
 import { toCleanJSON } from './helpers/utils';
+
+const REPO_NAME_VAR = 'Build.Repository.Name';
 
 export default async function prepareTask(endpoint: Endpoint, rootPath: string) {
   const scannerMode: ScannerMode = ScannerMode[tl.getInput('scannerMode')];
   const scanner = Scanner.getPrepareScanner(rootPath, scannerMode);
 
   const props: { [key: string]: string } = {};
+
+  if (endpoint.type === EndpointType.SonarCloud) {
+    await populateBranchAndPrProps(endpoint, props);
+    tl.debug(`[SQ] Branch and PR parameters: ${JSON.stringify(props)}`);
+  }
+
   tl
     .getDelimitedInput('extraProperties', '\n')
     .filter(keyValue => !keyValue.startsWith('#'))
@@ -26,4 +35,81 @@ export default async function prepareTask(endpoint: Endpoint, rootPath: string) 
   );
 
   await scanner.runPrepare();
+}
+
+async function populateBranchAndPrProps(endpoint: Endpoint, props: { [key: string]: string }) {
+  const collectionUrl = tl.getVariable('System.TeamFoundationCollectionUri');
+  const prId = tl.getVariable('System.PullRequest.PullRequestId');
+  const provider = tl.getVariable('Build.Repository.Provider');
+  if (prId) {
+    props['sonar.pullrequest.id'] = prId;
+    props['sonar.pullrequest.base'] = branchName(tl.getVariable('System.PullRequest.TargetBranch'));
+    props['sonar.pullrequest.branch'] = branchName(
+      tl.getVariable('System.PullRequest.SourceBranch')
+    );
+    if (provider === 'TfsGit') {
+      props['sonar.pullrequest.provider'] = 'vsts';
+      props['sonar.pullrequest.vsts.instanceUrl'] = collectionUrl;
+      props['sonar.pullrequest.vsts.project'] = tl.getVariable('System.TeamProject');
+      props['sonar.pullrequest.vsts.gitRepo'] = tl.getVariable(REPO_NAME_VAR);
+    } else if (provider === 'GitHub') {
+      props['sonar.pullrequest.provider'] = 'github';
+      props['sonar.pullrequest.github.repository'] = tl.getVariable(REPO_NAME_VAR);
+    } else {
+      tl.warning(`Unkwnow provider '${provider}'`);
+      props['sonar.scanner.skip'] = 'true';
+    }
+  } else {
+    const defaultBranch = await getDefaultBranch(provider, collectionUrl);
+    const currentBranch = tl.getVariable('Build.SourceBranch');
+    if (defaultBranch !== currentBranch) {
+      props['sonar.branch.name'] = branchName(currentBranch);
+    }
+  }
+}
+
+function branchName(fullName: string) {
+  if (fullName.startsWith('refs/heads/')) {
+    return fullName.substring('refs/heads/'.length);
+  }
+  return fullName;
+}
+
+/**
+ * Query the repo to get the full name of the default branch.
+ * @param collectionUrl
+ */
+async function getDefaultBranch(provider: string, collectionUrl: string) {
+  const DEFAULT = 'refs/heads/master';
+  if (provider !== 'TfsGit') {
+    return DEFAULT;
+  }
+  try {
+    const vsts: vm.WebApi = getWebApi(collectionUrl);
+    const gitApi = await vsts.getGitApi();
+    const repo = await gitApi.getRepository(
+      tl.getVariable(REPO_NAME_VAR),
+      tl.getVariable('System.TeamProject')
+    );
+    tl.debug(`Default branch of this repository is '${repo.defaultBranch}'`);
+    return repo.defaultBranch;
+  } catch (e) {
+    tl.warning("Unable to get default branch, defaulting to 'master': " + e);
+    return DEFAULT;
+  }
+}
+
+function getWebApi(collectionUrl: string): vm.WebApi {
+  const accessToken = getAuthToken();
+  const credentialHandler = vm.getBearerHandler(accessToken);
+  return new vm.WebApi(collectionUrl, credentialHandler);
+}
+
+function getAuthToken() {
+  const auth = tl.getEndpointAuthorization('SYSTEMVSSCONNECTION', false);
+  if (auth.scheme.toLowerCase() === 'oauth') {
+    return auth.parameters['AccessToken'];
+  } else {
+    throw new Error('Unable to get credential to perform rest API calls');
+  }
 }
