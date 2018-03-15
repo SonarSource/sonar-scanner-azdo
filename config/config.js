4f0d57a0eb596443e5921617cdcfabe2161954bf
Michal Barczyk <michal.barczyk@sonarsource.com>
2018-03-15 16:45:33 +0100

    update links to released version of the scanner

--- config/config.js
+++ config/config.js
@@ -1,22 +1,11 @@
-const msBuildVersion = '4.0.2.892';
-const cliVersion = '3.0.3.778'; // Has to be the same version as the one embedded in the Scanner for MSBuild
+const msBuildVersion = '4.1.0.1148';
+const cliVersion = '3.1.0.1141'; // Has to be the same version as the one embedded in the Scanner for MSBuild
 
-// const scannerUrlCommon = `https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/${msBuildVersion}/sonar-scanner-msbuild-${msBuildVersion}`
-// classicUrl: `${scannerUrlCommon}-net46.zip`,
-// dotnetUrl:  `${scannerUrlCommon}-netcoreapp2.0.zip`
-
-// `https://repox.sonarsource.com/sonarsource/org/sonarsource/scanner/msbuild/sonar-scanner-msbuild/4.1.0.1103/sonar-scanner-msbuild-4.1.0.1103-net46.zip`,
-// `https://repox.sonarsource.com/sonarsource/org/sonarsource/scanner/msbuild/sonar-scanner-msbuild/4.1.0.1103/sonar-scanner-msbuild-4.1.0.1103-netcoreapp2.0.zip`
+const scannerUrlCommon = `https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/${msBuildVersion}/sonar-scanner-msbuild-${msBuildVersion}`
 
 exports.scanner = {
   msBuildVersion,
   cliVersion,
-
-  //TODO: use values from lines 4-6 instead!
-
-  //classicUrl: `http://michal-barczyk/sonarqube-scanner-msbuild-net46.zip`,
-  //dotnetUrl:  `http://michal-barczyk/sonarqube-scanner-msbuild-netcoreapp2.0.zip`
-
-  classicUrl: `https://repox.sonarsource.com/sonarsource/org/sonarsource/scanner/msbuild/sonar-scanner-msbuild/4.1.0.1103/sonar-scanner-msbuild-4.1.0.1103-net46.zip`,
-  dotnetUrl:  `https://repox.sonarsource.com/sonarsource/org/sonarsource/scanner/msbuild/sonar-scanner-msbuild/4.1.0.1103/sonar-scanner-msbuild-4.1.0.1103-netcoreapp2.0.zip`
+  classicUrl: `${scannerUrlCommon}-net46.zip`,
+  dotnetUrl:  `${scannerUrlCommon}-netcoreapp2.0.zip`
 };
