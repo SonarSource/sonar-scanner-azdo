eaf94dcffc49456967cc37321a21075bbf812e8f
Julien HENRY <julien.henry@sonarsource.com>
2018-03-23 11:21:34 +0100

    Prepare release of SC 1.1.2 and SQ 4.1.1

--- config/config.js
+++ config/config.js
@@ -1,13 +1,13 @@
-const msBuildVersion = '4.1.0.1148';
+const msBuildVersion = '4.1.1.1164';
 const cliVersion = '3.1.0.1141'; // Has to be the same version as the one embedded in the Scanner for MSBuild
 
-const scannerUrlCommon
-    = `https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/${msBuildVersion}/`
-      + `sonar-scanner-msbuild-${msBuildVersion}`;
+const scannerUrlCommon =
+  `https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/${msBuildVersion}/` +
+  `sonar-scanner-msbuild-${msBuildVersion}`;
 
 exports.scanner = {
   msBuildVersion,
   cliVersion,
   classicUrl: `${scannerUrlCommon}-net46.zip`,
-  dotnetUrl:  `${scannerUrlCommon}-netcoreapp2.0.zip`
+  dotnetUrl: `${scannerUrlCommon}-netcoreapp2.0.zip`
 };
