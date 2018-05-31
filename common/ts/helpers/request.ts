8b47f804a9af4892f4fbde7006d62d7524f404d2
Julien HENRY <julien.henry@sonarsource.com>
2018-05-31 10:19:52 +0200

    VSTS-156 Only set variables if SQ >= 7.2

--- common/ts/helpers/request.ts
+++ common/ts/helpers/request.ts
@@ -1,4 +1,5 @@
 import * as request from 'request';
+import * as semver from 'semver';
 import * as tl from 'vsts-task-lib/task';
 import Endpoint from '../sonarqube/Endpoint';
 
@@ -6,7 +7,7 @@ interface RequestData {
   [x: string]: any;
 }
 
-export function getJSON(endpoint: Endpoint, path: string, query?: RequestData): Promise<any> {
+function get(endpoint: Endpoint, path: string, isJson: boolean, query?: RequestData): Promise<any> {
   tl.debug(`[SQ] API GET: '${path}' with query "${JSON.stringify(query)}"`);
   return new Promise((resolve, reject) => {
     const options: request.CoreOptions = {
@@ -21,23 +22,44 @@ export function getJSON(endpoint: Endpoint, path: string, query?: RequestData): 
         method: 'GET',
         baseUrl: endpoint.url,
         uri: path,
-        json: true,
+        json: isJson,
         ...options
       },
       (error, response, body) => {
         if (error) {
-          const errMsg = `[SQ] API GET '${path}' failed, error was: ${JSON.stringify(error)}`;
-          tl.debug(errMsg);
-          return reject(new Error(errMsg));
+          return logAndReject(
+            reject,
+            `[SQ] API GET '${path}' failed, error was: ${JSON.stringify(error)}`
+          );
         }
-        tl.debug(`Response: ${response.statusCode} Body: "${JSON.stringify(body)}"`);
+        tl.debug(
+          `Response: ${response.statusCode} Body: "${isString(body) ? body : JSON.stringify(body)}"`
+        );
         if (response.statusCode < 200 || response.statusCode >= 300) {
-          const errMsg = `[SQ] API GET '${path}' failed, status code was: ${response.statusCode}`;
-          tl.debug(errMsg);
-          return reject(new Error(errMsg));
+          return logAndReject(
+            reject,
+            `[SQ] API GET '${path}' failed, status code was: ${response.statusCode}`
+          );
         }
-        return resolve(body || {});
+        return resolve(body || (isJson ? {} : ''));
       }
     );
   });
+}
+
+function isString(x) {
+  return Object.prototype.toString.call(x) === '[object String]';
+}
+
+export function getJSON(endpoint: Endpoint, path: string, query?: RequestData): Promise<any> {
+  return get(endpoint, path, true, query);
+}
+
+export function getServerVersion(endpoint: Endpoint): Promise<semver.SemVer> {
+  return get(endpoint, '/api/server/version', false).then(semver.coerce);
+}
+
+function logAndReject(reject, errMsg) {
+  tl.debug(errMsg);
+  return reject(new Error(errMsg));
 }
