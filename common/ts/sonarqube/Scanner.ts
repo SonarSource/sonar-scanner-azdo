52996c4701bf400a2713cea3cdef895fcc6676d4
Julien HENRY <julien.henry@sonarsource.com>
2018-06-07 17:38:02 +0200

    VSTS-158 Automatically provide sonar.verbose=true when debug=true

--- common/ts/sonarqube/Scanner.ts
+++ common/ts/sonarqube/Scanner.ts
@@ -59,6 +59,10 @@ export default class Scanner {
       tl.command('task.logissue', { type: 'error' }, data);
     });
   }
+
+  isDebug() {
+    return tl.getVariable('system.debug') === 'true';
+  }
 }
 
 interface ScannerCLIData {
@@ -96,6 +100,9 @@ export class ScannerCLI extends Scanner {
     }
     const scannerRunner = tl.tool(scannerCliScript);
     this.logIssueOnBuildSummaryForStdErr(scannerRunner);
+    if (this.isDebug()) {
+      scannerRunner.arg('-X');
+    }
     await scannerRunner.exec();
   }
 
@@ -156,6 +163,9 @@ export class ScannerMSBuild extends Scanner {
     scannerRunner.arg('begin');
     scannerRunner.arg('/k:' + this.data.projectKey);
     this.logIssueOnBuildSummaryForStdErr(scannerRunner);
+    if (this.isDebug()) {
+      scannerRunner.arg('/d:sonar.verbose=true');
+    }
     await scannerRunner.exec();
   }
 
