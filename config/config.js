957dfd3a6352d4a3c0004aedf77e73b0787f4a26
Julien HENRY <julien.henry@sonarsource.com>
2018-06-14 14:53:26 +0200

    VSTS-157 Update SonarScanner for MSBuild to 4.3.1

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-const msBuildVersion = '4.3.0.1333';
+const msBuildVersion = '4.3.1.1372';
 const cliVersion = '3.2.0.1227'; // Has to be the same version as the one embedded in the Scanner for MSBuild
 
 const scannerUrlCommon =
