60673927005c96b6324d3ae3b8ebcf1652e1bf34
Mickaël <mickael.caro@sonarsource.com>
2019-07-29 16:26:33 +0200

    VSTS-201 - Add mock to a method to avoid error on QA (AzDO)

--- common/ts/__tests__/publish-task-test.ts
+++ common/ts/__tests__/publish-task-test.ts
@@ -136,6 +136,8 @@ it('task should not fail the task even if all ceTasks timeout', async () => {
     .spyOn(serverUtils, 'publishBuildSummary')
     .mockImplementation(() => null);
 
+  jest.spyOn(Metrics, 'getAllMetrics').mockImplementation(() => METRICS);
+
   await publishTask.default(EndpointType.SonarCloud);
 
   expect(serverUtils.publishBuildSummary).toHaveBeenCalledTimes(1);
