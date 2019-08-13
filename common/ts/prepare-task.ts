a3d60f8117de1919d49198ae8922d68be596808e
samattridge <sam@tridge.co.uk>
2019-08-13 13:25:13 +0100

    Cope with SONAR_SCANNER_OPTS not being defined.

--- common/ts/prepare-task.ts
+++ common/ts/prepare-task.ts
@@ -54,7 +54,9 @@ export default async function prepareTask(endpoint: Endpoint, rootPath: string) 
 
   if (scannerMode === ScannerMode.CLI) {
     let actualSonarOpts = tl.getVariable('SONAR_SCANNER_OPTS');
-    actualSonarOpts = actualSonarOpts.concat(' ', scanner.toCliProps()).trim();
+    if (actualSonarOpts != undefined) {
+      actualSonarOpts = actualSonarOpts.concat(' ', scanner.toCliProps()).trim();
+    }
     tl.setVariable('SONAR_SCANNER_OPTS', actualSonarOpts);
   }
 
