0547c44c57de8fccb45c8470fd54baa2e52fc8a3
Mickaël <mickael.caro@sonarsource.com>
2020-05-05 08:39:28 +0200

    VSTS-227 - Bumped version of S4MSB to 4.9.0.17385 + ScannerCLI to 4.3.0.2102 + Bumped extension versions.

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
-const msBuildVersion = '4.8.0.12008';
-const cliVersion = '4.2.0.1873'; // Has to be the same version as the one embedded in the Scanner for MSBuild
+const msBuildVersion = '4.9.0.17385';
+const cliVersion = '4.3.0.2102'; // Has to be the same version as the one embedded in the Scanner for MSBuild
 
 const scannerUrlCommon =
   `https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/${msBuildVersion}/` +
