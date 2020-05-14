2918ffc2ade6ebd29daec48dda2c32bb30e26812
Mickaël <mickael.caro@sonarsource.com>
2020-05-14 08:25:01 +0200

    VSTS-232 handle Java warning as a build warning, as part of MMF-2035

--- common/ts/analyze-task.ts
+++ common/ts/analyze-task.ts
@@ -1,14 +1,14 @@
 import * as tl from "azure-pipelines-task-lib/task";
 import Scanner, { ScannerMode } from "./sonarqube/Scanner";
 
-export default async function analyzeTask(rootPath: string) {
+export default async function analyzeTask(rootPath: string, isSonarCloud: boolean = false) {
   const scannerMode: ScannerMode = ScannerMode[tl.getVariable("SONARQUBE_SCANNER_MODE")];
   if (!scannerMode) {
     throw new Error(
       "[SQ] The 'Prepare Analysis Configuration' task was not executed prior to this task"
     );
   }
-
+  Scanner.setIsSonarCloud(isSonarCloud);
   const scanner = Scanner.getAnalyzeScanner(rootPath, scannerMode);
   await scanner.runAnalysis();
 }
