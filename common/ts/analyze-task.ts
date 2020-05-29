250ec14d472a2df4c9ecc25350b0273fee2e334e
Mickaël <mickael.caro@sonarsource.com>
2020-05-29 15:09:22 +0200

    VSTS-235 If available, use JAVA_HOME_11_X64 as JAVA_HOME

--- common/ts/analyze-task.ts
+++ common/ts/analyze-task.ts
@@ -9,6 +9,18 @@ export default async function analyzeTask(rootPath: string, isSonarCloud: boolea
     );
   }
   Scanner.setIsSonarCloud(isSonarCloud);
+  const originalJavaHome = tl.getVariable("JAVA_HOME");
+  const java11Path = tl.getVariable("JAVA_HOME_11_X64");
+  let hasTurnedToJava11 = false;
+  if (java11Path) {
+    tl.debug("Java 11 path has been detected, switching to it for the analysis.");
+    tl.setVariable("JAVA_HOME", java11Path);
+    hasTurnedToJava11 = true;
+  }
   const scanner = Scanner.getAnalyzeScanner(rootPath, scannerMode);
   await scanner.runAnalysis();
+  if (hasTurnedToJava11) {
+    tl.debug("Reverting JAVA_HOME to its initial path.");
+    tl.setVariable("JAVA_HOME", originalJavaHome);
+  }
 }
