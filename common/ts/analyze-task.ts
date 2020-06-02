ea203f82e87f816036a1173e99a78f4990fff605
mickael-caro-sonarsource <mickael.caro@sonarsource.com>
2020-06-02 09:01:16 +0200

    VSTS-235 If available, use JAVA_HOME_11_X64 as JAVA_HOME (#117)
    * VSTS-235 If available, use JAVA_HOME_11_X64 as JAVA_HOME

--- common/ts/analyze-task.ts
+++ common/ts/analyze-task.ts
@@ -1,5 +1,8 @@
 import * as tl from "azure-pipelines-task-lib/task";
 import Scanner, { ScannerMode } from "./sonarqube/Scanner";
+import JavaVersionResolver from "./helpers/java-version-resolver";
+
+const JAVA_11_PATH_ENV_NAME = "JAVA_HOME_11_X64";
 
 export default async function analyzeTask(rootPath: string, isSonarCloud: boolean = false) {
   const scannerMode: ScannerMode = ScannerMode[tl.getVariable("SONARQUBE_SCANNER_MODE")];
@@ -9,6 +12,8 @@ export default async function analyzeTask(rootPath: string, isSonarCloud: boolea
     );
   }
   Scanner.setIsSonarCloud(isSonarCloud);
+  JavaVersionResolver.setJavaHomeToIfAvailable(JAVA_11_PATH_ENV_NAME);
   const scanner = Scanner.getAnalyzeScanner(rootPath, scannerMode);
   await scanner.runAnalysis();
+  JavaVersionResolver.revertJavaHomeToOriginal();
 }
