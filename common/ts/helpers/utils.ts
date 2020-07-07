b08a4d71170b05fd74441bcd73a2e8f7875995c9
mickael-caro-sonarsource <mickael.caro@sonarsource.com>
2020-07-07 09:11:59 +0200

    VSTS-239 Revert Scanner CLI identity (#126)

--- common/ts/helpers/utils.ts
+++ common/ts/helpers/utils.ts
@@ -1,5 +1,3 @@
-import * as path from "path";
-import * as fs from "fs-extra";
 import * as tl from "azure-pipelines-task-lib/task";
 
 export const PROP_NAMES = {
@@ -29,14 +27,4 @@ export function setIfNotEmpty(props: { [key: string]: string }, key: string, val
 
 export function isWindows() {
   return tl.osType().match(/^Win/);
-}
-
-export function getTaskVersion(taskJsonPath: string): string {
-  const fullPath = path.join(taskJsonPath, "task.json");
-  if (!fs.existsSync(fullPath)) {
-    return "1.0.0";
-  }
-  const fileContent = fs.readFileSync(path.join(taskJsonPath, "task.json"), "utf8");
-  const jsonContent = JSON.parse(fileContent);
-  return `${jsonContent.version.Major}.${jsonContent.version.Minor}.${jsonContent.version.Patch}`;
 }
