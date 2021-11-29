2dd3ef37da443f66e66f34d82c94ed5d45136ee6
Mickaël Caro <mickael.caro@sonarsource.com>
2021-11-29 18:10:16 +0100

    VSTS-263 Fix SSF-194 (#171)

--- common/ts/prepare-task.ts
+++ common/ts/prepare-task.ts
@@ -4,7 +4,7 @@ import * as tl from "azure-pipelines-task-lib/task";
 import { Guid } from "guid-typescript";
 import Endpoint, { EndpointType } from "./sonarqube/Endpoint";
 import Scanner, { ScannerMode } from "./sonarqube/Scanner";
-import { toCleanJSON } from "./helpers/utils";
+import { toCleanJSON, sanitizeVariable } from "./helpers/utils";
 import { getServerVersion } from "./helpers/request";
 import * as azdoApiUtils from "./helpers/azdo-api-utils";
 import { REPORT_TASK_NAME, SONAR_TEMP_DIRECTORY_NAME } from "./sonarqube/TaskReport";
@@ -45,16 +45,17 @@ export default async function prepareTask(endpoint: Endpoint, rootPath: string) 
 
   tl.setVariable("SONARQUBE_SCANNER_MODE", scannerMode);
   tl.setVariable("SONARQUBE_ENDPOINT", endpoint.toJson(), true);
-  tl.setVariable(
-    "SONARQUBE_SCANNER_PARAMS",
-    toCleanJSON({
-      ...endpoint.toSonarProps(),
-      ...scanner.toSonarProps(),
-      ...props,
-    })
-  );
+
+  const jsonParams = toCleanJSON({
+    ...endpoint.toSonarProps(),
+    ...scanner.toSonarProps(),
+    ...props,
+  });
+
+  tl.setVariable("SONARQUBE_SCANNER_PARAMS", jsonParams);
 
   await scanner.runPrepare();
+  tl.setVariable("SONARQUBE_SCANNER_PARAMS", sanitizeVariable(jsonParams));
 }
 
 async function branchFeatureSupported(endpoint) {
