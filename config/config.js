841a403bc1b4e470fc2361fa07fef95cf000fdd6
Mickael Caro <mickael.caro@sonarsource.com>
2022-02-01 10:58:20 +0100

    VSTS-267 Bump SonarScanner for .NET to 5.4.1.41282

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-const msBuildVersion = "5.4.0.40033";
+const msBuildVersion = "5.4.1.41282";
 const cliVersion = "4.6.2.2472"; // Has to be the same version as the one embedded in the Scanner for MSBuild
 
 const scannerUrlCommon =
