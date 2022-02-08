d389c22cc8aabede1867b221b8008ab8d3c9dc3d
Mickael Caro <mickael.caro@sonarsource.com>
2022-02-08 11:01:53 +0100

    fix formatting

--- commonv5/ts/helpers/__tests__/azdo-api-utils-test.ts
+++ commonv5/ts/helpers/__tests__/azdo-api-utils-test.ts
@@ -4,16 +4,16 @@ import * as vm from "azure-devops-node-api";
 import * as azdoApiUtils from "../azdo-api-utils";
 
 it("should build jsonpath body properly", async () => {
-  jest.spyOn(tl, "getVariable").mockReturnValueOnce("https://collection-uri.com") //System.TeamFoundationCollectionUri
-  jest.spyOn(tl, "getVariable").mockReturnValueOnce("1234") //System.TeamProjectId
-  jest.spyOn(tl, "getVariable").mockReturnValueOnce("4567") //Build.BuildId
-  jest.spyOn(tl, "getEndpointAuthorization").mockReturnValue({ 'scheme': 'oauth', 'parameters': {}});
+  jest.spyOn(tl, "getVariable").mockReturnValueOnce("https://collection-uri.com"); //System.TeamFoundationCollectionUri
+  jest.spyOn(tl, "getVariable").mockReturnValueOnce("1234"); //System.TeamProjectId
+  jest.spyOn(tl, "getVariable").mockReturnValueOnce("4567"); //Build.BuildId
+  jest.spyOn(tl, "getEndpointAuthorization").mockReturnValue({ scheme: "oauth", parameters: {} });
   jest.spyOn(azdoApiUtils, "getAuthToken").mockImplementation(() => null);
 
   const webApi = new vm.WebApi("http://vsts.net", null);
 
   jest.spyOn(azdoApiUtils, "getWebApi").mockImplementation(() => webApi);
-  jest.spyOn(tl, "debug").mockImplementation(() => null)
+  jest.spyOn(tl, "debug").mockImplementation(() => null);
 
   const jsonAsString = `[{\"op\":${Operation.Add},\"path\":\"/sonarglobalqualitygate\",\"value\":\"test\"}]`;
 
