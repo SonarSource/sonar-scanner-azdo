b622d4fbb822ef3761d3e040ae5e2ac5c3883e1f
Mickael Caro <mickael.caro@sonarsource.com>
2022-02-24 08:22:38 +0100

    fix conflicts

--- common/ts/helpers/__tests__/azdo-api-utils-test.ts
+++ common/ts/helpers/__tests__/azdo-api-utils-test.ts
@@ -1,33 +1,29 @@
-import * as tl from "azure-pipelines-task-lib/task";
-import { Operation } from "azure-devops-node-api/interfaces/common/VSSInterfaces";
-import * as vm from "azure-devops-node-api";
-import * as azdoApiUtils from "../azdo-api-utils";
+import * as tl from 'azure-pipelines-task-lib/task';
+import { Operation } from 'azure-devops-node-api/interfaces/common/VSSInterfaces';
+import * as vm from 'azure-devops-node-api';
+import * as azdoApiUtils from '../azdo-api-utils';
 
-it("should build jsonpath body properly", () => {
-  jest.spyOn(tl, "getEndpointAuthorization").mockImplementation(() => null);
-  jest.spyOn(azdoApiUtils, "getAuthToken").mockImplementation(() => null);
+it('should build jsonpath body properly', () => {
+  jest.spyOn(tl, 'getEndpointAuthorization').mockImplementation(() => null);
+  jest.spyOn(azdoApiUtils, 'getAuthToken').mockImplementation(() => null);
 
-  const webApi = new vm.WebApi("http://vsts.net", null);
+  const webApi = new vm.WebApi('http://vsts.net', null);
 
-  jest.spyOn(tl, "debug").mockImplementation(() => null);
-  jest.spyOn(azdoApiUtils, "getWebApi").mockImplementation(() => webApi);
+  jest.spyOn(tl, 'debug').mockImplementation(() => null);
+  jest.spyOn(azdoApiUtils, 'getWebApi').mockImplementation(() => webApi);
 
-  const jsonAsString = `[{\"op\":${Operation.Add},\"path\":\"/sonarglobalqualitygate\",\"value\":\"test\"}]`;
+  const jsonAsString = `[{"op":${Operation.Add},"path":"/sonarglobalqualitygate","value":"test"}]`;
 
   const properties: azdoApiUtils.IPropertyBag[] = [];
 
   properties.push({
-    propertyName: "sonarglobalqualitygate",
-    propertyValue: "test",
+    propertyName: 'sonarglobalqualitygate',
+    propertyValue: 'test',
   });
 
-<<<<<<< HEAD
   azdoApiUtils.addBuildProperty(properties).then(() => {
     return;
   });
-=======
-  azdoApiUtils.addBuildProperty(properties).then(() => {});
->>>>>>> parent of 904da3a (Changed double quotes to single.)
 
   expect(tl.debug).toHaveBeenCalledWith(jsonAsString);
 });
