0b0f8c85efa79ff6c51e43c8ffdfd15ef563f4a9
Mickael Caro <mickael.caro@sonarsource.com>
2022-02-24 08:26:33 +0100

    fix double quotes

--- common/ts/helpers/__tests__/azdo-api-utils-test.ts
+++ common/ts/helpers/__tests__/azdo-api-utils-test.ts
@@ -1,24 +1,24 @@
-import * as tl from 'azure-pipelines-task-lib/task';
-import { Operation } from 'azure-devops-node-api/interfaces/common/VSSInterfaces';
-import * as vm from 'azure-devops-node-api';
-import * as azdoApiUtils from '../azdo-api-utils';
+import * as tl from "azure-pipelines-task-lib/task";
+import { Operation } from "azure-devops-node-api/interfaces/common/VSSInterfaces";
+import * as vm from "azure-devops-node-api";
+import * as azdoApiUtils from "../azdo-api-utils";
 
-it('should build jsonpath body properly', () => {
-  jest.spyOn(tl, 'getEndpointAuthorization').mockImplementation(() => null);
-  jest.spyOn(azdoApiUtils, 'getAuthToken').mockImplementation(() => null);
+it("should build jsonpath body properly", () => {
+  jest.spyOn(tl, "getEndpointAuthorization").mockImplementation(() => null);
+  jest.spyOn(azdoApiUtils, "getAuthToken").mockImplementation(() => null);
 
-  const webApi = new vm.WebApi('http://vsts.net', null);
+  const webApi = new vm.WebApi("http://vsts.net", null);
 
-  jest.spyOn(tl, 'debug').mockImplementation(() => null);
-  jest.spyOn(azdoApiUtils, 'getWebApi').mockImplementation(() => webApi);
+  jest.spyOn(tl, "debug").mockImplementation(() => null);
+  jest.spyOn(azdoApiUtils, "getWebApi").mockImplementation(() => webApi);
 
   const jsonAsString = `[{"op":${Operation.Add},"path":"/sonarglobalqualitygate","value":"test"}]`;
 
   const properties: azdoApiUtils.IPropertyBag[] = [];
 
   properties.push({
-    propertyName: 'sonarglobalqualitygate',
-    propertyValue: 'test',
+    propertyName: "sonarglobalqualitygate",
+    propertyValue: "test",
   });
 
   azdoApiUtils.addBuildProperty(properties).then(() => {
