c74dd40b2eb9e39c58e40e9b4736dc966662ee6d
Mickael Caro <mickael.caro@sonarsource.com>
2022-06-07 09:21:36 +0200

    VSTS-281 fix azp version computation

--- azp/helpers.ps1
+++ azp/helpers.ps1
@@ -1,6 +1,6 @@
 function Get-ExtensionVersion(
     [Parameter(Mandatory = $true, Position = 0)][string]$manifestPath,
-    [Parameter(Mandatory = $true, Position = 1)][string]$product)   {
+    [Parameter(Mandatory = $true, Position = 1)][string]$product) {
 
     Write-Host "Reading the ${product} extension version from '${manifestPath}' ..."
 
@@ -12,23 +12,32 @@ function Get-ExtensionVersion(
     return $version
 }
 
+function Format-SnapshotVersion(
+    [Parameter(Mandatory = $true, Position = 0)][string]$version) {
+    if ($version -contains "-SNAPSHOT" && -Not [string]::IsNullOrEmpty($env:BUILD_BUILDID)) {
+        Write-Host 'condition met'
+        $version = $version.replace("-SNAPSHOT", ".$env:BUILD_BUILDID")
+    }
+    return $version
+}
+
 function Get-AzureDevopsTaskVersion(
     [Parameter(Mandatory = $true, Position = 0)][string]$basePath,
     [Parameter(Mandatory = $true, Position = 1)][string]$product,
     [Parameter(Mandatory = $true, Position = 2)][string]$task,
-    [Parameter(Mandatory = $true, Position = 3)][string]$version){
-        $extPath = Join-Path -Path $basePath -ChildPath $product -AdditionalChildPath "tasks"
+    [Parameter(Mandatory = $true, Position = 3)][string]$version) {
+    $extPath = Join-Path -Path $basePath -ChildPath $product -AdditionalChildPath "tasks"
 
-        $manifestFileName = "task.json"
+    $manifestFileName = "task.json"
 
-        $taskVersionPath = Join-Path -Path $extPath -ChildPath $task -AdditionalChildPath $version
+    $taskVersionPath = Join-Path -Path $extPath -ChildPath $task -AdditionalChildPath $version
 
-        $version = Get-TaskVersion(Join-Path -Path $taskVersionPath $manifestFileName) 
+    $version = Get-TaskVersion(Join-Path -Path $taskVersionPath $manifestFileName) 
 
-        return $version
+    return $version
 }
 
-function Get-TaskVersion([Parameter(Mandatory = $true, Position = 0)][string]$filePath){
+function Get-TaskVersion([Parameter(Mandatory = $true, Position = 0)][string]$filePath) {
     $fileContent = Get-Content "$filePath" | ConvertFrom-Json
 
     $major = $fileContent.version.Major
