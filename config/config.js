6a20ec350a8a1c0a515fdb3805db9a60b4aa983d
Mickaël Caro <mickael.caro@sonarsource.com>
2023-03-16 07:40:46 +0100

    VSTS-299 Bumped Scanner for .NET version to 5.12.0.64969

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-const msBuildVersion = "5.11.0.60783";
+const msBuildVersion = "5.12.0.64969";
 const cliVersion = "4.8.0.2856"; // Has to be the same version as the one embedded in the Scanner for MSBuild
 
 const scannerUrlCommon =
