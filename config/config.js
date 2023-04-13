5c23ba9bb27dfb5630c47e6718cc1c0ae4787abd
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2023-04-13 11:18:41 +0200

    VSTS-300 Bump Scanner for .NET 5.13.0.66756 (#250)

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-const msBuildVersion = "5.12.0.64969";
+const msBuildVersion = "5.13.0.66756";
 const cliVersion = "4.8.0.2856"; // Has to be the same version as the one embedded in the Scanner for MSBuild
 
 const scannerUrlCommon =
