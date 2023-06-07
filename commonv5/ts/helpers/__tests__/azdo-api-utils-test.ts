b7605fdd680198af8d5548ce20e211bac2d200d4
7PH <benjamin.raymond@sonarsource.com>
2023-06-07 15:47:37 +0200

    VSTS-290 Allow sonar.scanner.metadataFilePath property override

--- commonv5/ts/helpers/__tests__/azdo-api-utils-test.ts
+++ commonv5/ts/helpers/__tests__/azdo-api-utils-test.ts
@@ -1,6 +1,6 @@
-import * as tl from "azure-pipelines-task-lib/task";
-import { Operation } from "azure-devops-node-api/interfaces/common/VSSInterfaces";
 import * as vm from "azure-devops-node-api";
+import { Operation } from "azure-devops-node-api/interfaces/common/VSSInterfaces";
+import * as tl from "azure-pipelines-task-lib/task";
 import * as azdoApiUtils from "../azdo-api-utils";
 
 it("should build jsonpath body properly", async () => {
@@ -27,4 +27,17 @@ it("should build jsonpath body properly", async () => {
   await azdoApiUtils.addBuildProperty(properties).then(() => {});
 
   expect(tl.debug).toHaveBeenCalledWith(jsonAsString);
+});
+
+it("should parse extra properties correctly", () => {
+  jest.spyOn(tl, "getDelimitedInput").mockReturnValueOnce(
+    `# Additional properties that will be passed to the scanner, 
+# Put one key=value per line, example:
+# sonar.exclusions=**/*.bin
+sonar.scanner.metadataFilePath=/tmp/report-task-debug.txt
+sonar.scanner.metadataFilePath=/tmp/report-task-debug-override.txt`.split("\n")
+  ); //extraProperties
+  const props = azdoApiUtils.parseScannerExtraProperties();
+
+  expect(props["sonar.scanner.metadataFilePath"]).toBe("/tmp/report-task-debug-override.txt");
 });
