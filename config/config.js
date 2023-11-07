8267c4442b2afeacb2652aef2c5bf167fe023c33
7PH <benjamin.raymond@sonarsource.com>
2023-11-07 17:16:25 +0100

    VSTS-328 Allow local scanner archives to be used at build time

--- config/config.js
+++ config/config.js
@@ -1,13 +1,13 @@
 const msBuildVersion = "5.14.0.78575";
 const cliVersion = "4.8.1.3023"; // Has to be the same version as the one embedded in the Scanner for MSBuild
 
-const scannerUrlCommon =
-  `https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/${msBuildVersion}/` +
-  `sonar-scanner-msbuild-${msBuildVersion}`;
+const scannersLocation = `https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/${msBuildVersion}/`;
+const classicScannerFilename = `sonar-scanner-msbuild-${msBuildVersion}-net46.zip`;
+const dotnetScannerFilename = `sonar-scanner-msbuild-${msBuildVersion}-netcoreapp3.0.zip`;
 
 exports.scanner = {
   msBuildVersion,
   cliVersion,
-  classicUrl: `${scannerUrlCommon}-net46.zip`,
-  dotnetUrl: `${scannerUrlCommon}-netcoreapp3.0.zip`,
+  classicUrl: scannersLocation + classicScannerFilename,
+  dotnetUrl: scannersLocation + dotnetScannerFilename,
 };
