0d0628eee269e8237889ace4b4c44e18fd66f887
7PH <benjamin.raymond@sonarsource.com>
2024-03-15 14:33:28 +0100

    VSTS-335 Drop dead powershell code

--- azp/helpers.ps1
+++ azp/helpers.ps1
@@ -30,29 +30,3 @@ function Replace-Version([Parameter(Mandatory = $true, Position = 0)][string]$ma
 
     return $formattedVersion
 }
-
-function Get-AzureDevopsTaskVersion(
-    [Parameter(Mandatory = $true, Position = 0)][string]$basePath,
-    [Parameter(Mandatory = $true, Position = 1)][string]$product,
-    [Parameter(Mandatory = $true, Position = 2)][string]$task,
-    [Parameter(Mandatory = $true, Position = 3)][string]$version) {
-    $extPath = Join-Path -Path $basePath -ChildPath $product -AdditionalChildPath "tasks"
-
-    $manifestFileName = "task.json"
-
-    $taskVersionPath = Join-Path -Path $extPath -ChildPath $task -AdditionalChildPath $version
-
-    $version = Get-TaskVersion(Join-Path -Path $taskVersionPath $manifestFileName) 
-
-    return $version
-}
-
-function Get-TaskVersion([Parameter(Mandatory = $true, Position = 0)][string]$filePath) {
-    $fileContent = Get-Content "$filePath" | ConvertFrom-Json
-
-    $major = $fileContent.version.Major
-    $minor = $fileContent.version.Minor
-    $patch = $fileContent.version.Patch
-
-    return "$major.$minor.$patch"
-}
