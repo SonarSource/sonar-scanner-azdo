fde8a7db62eeaed2f74b3b5325e881eb0b46b373
7PH <b.raymond@protonmail.com>
2024-05-15 16:42:29 +0200

    VSTS-335 Drop PowerShell SQ tasks (V3)

--- config/paths.js
+++ config/paths.js
@@ -32,18 +32,12 @@ exports.paths = {
     classicScanner: path.join(buildPath, 'tmp', 'classic-sonar-scanner-msbuild'),
     dotnetScanner: path.join(buildPath, 'tmp', 'dotnet-sonar-scanner-msbuild')
   },
-  common: {
-    old: path.join(commonPath, 'powershell'),
-    new: path.join(commonPath, 'ts')
-  },
-  commonv5: {
-    new: path.join(commonv5Path, 'ts')
-  },
+  common: path.join(commonPath, 'ts'),
+  commonv5: path.join(commonv5Path, 'ts'),
   extensions: {
     root: extensionsPath,
     tasks: {
       root: path.join(extensionsPath, '**', 'tasks'),
-      old: path.join(extensionsPath, '**', 'tasks', '**', 'old'),
       scv1: path.join(extensionsPath, '**', 'tasks', '**', 'v1'),
       v4: path.join(extensionsPath, '**', 'tasks', '**', 'v4'),
       v5: path.join(extensionsPath, '**', 'tasks', '**', 'v5')
