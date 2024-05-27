df10079ff2996f2946aaa183cddbb1d49221a897
Lucas Paulger <lucas.paulger@sonarsource.com>
2024-05-27 13:33:38 +0300

    install deps for old versions

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,4 +6,6 @@ cd $GIT_ROOT && npm install
 
 cd $GIT_ROOT/src/common/latest/ && npm install
 cd $GIT_ROOT/src/common/sonarqube-v4/ && npm install
+cd $GIT_ROOT/src/common/sonarqube-v5/ && npm install
+cd $GIT_ROOT/src/common/sonarcloud-v1/ && npm install
 
