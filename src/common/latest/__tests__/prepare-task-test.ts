3b2cdceacbab7590986c8a5652684e931de1d603
Lucas Paulger <lucas.paulger@sonarsource.com>
2024-05-27 13:24:16 +0300

    add test coverage for downloading MSBuild scanner

--- src/common/latest/__tests__/prepare-task-test.ts
+++ src/common/latest/__tests__/prepare-task-test.ts
@@ -1,4 +1,5 @@
 import * as tl from "azure-pipelines-task-lib/task";
+import * as toolLib from "azure-pipelines-tool-lib/tool";
 import { Guid } from "guid-typescript";
 import * as path from "path";
 import { SemVer } from "semver";
@@ -6,6 +7,8 @@ import * as request from "../helpers/request";
 import * as prept from "../prepare-task";
 import Endpoint, { EndpointType } from "../sonarqube/Endpoint";
 import TaskReport from "../sonarqube/TaskReport";
+import Scanner, { ScannerCLI } from "../sonarqube/Scanner";
+import { JdkVersionSource, TaskVariables } from "../helpers/constants";
 
 beforeEach(() => {
   jest.restoreAllMocks();
@@ -59,4 +62,31 @@ it("should build report task path from variables", () => {
   const actual = TaskReport.getDefaultPath();
 
   expect(actual).toEqual(reportFullPath);
+});
+
+it("should download the default version of the scanner", async () => {
+  //JAVA_HOME
+  jest.spyOn(tl, "getInput").mockImplementationOnce(() => JdkVersionSource.JavaHome);
+  jest.spyOn(tl, "getEndpointUrl").mockImplementation(() => "https://localhost:9000");
+  jest.spyOn(request, "getServerVersion").mockResolvedValue(new SemVer("10.5.0"));
+  jest.spyOn(TaskReport, "getDefaultPath").mockReturnValue("report-task.txt");
+  jest.spyOn(toolLib, "downloadTool").mockResolvedValue("path/to/zip-scanner");
+  jest.spyOn(toolLib, "extractZip").mockResolvedValue("path/to/unzip-scanner");
+  jest.spyOn(tl, "setVariable");
+
+  const scanner = new ScannerCLI(__dirname, { projectSettings: "scanner.properties" });
+
+  jest.spyOn(Scanner, "getPrepareScanner").mockImplementation(() => scanner);
+
+  await prept.prepareTask(EndpointType.SonarQube);
+
+  expect(Scanner.getPrepareScanner).toHaveBeenCalled();
+  expect(toolLib.downloadTool).toHaveBeenCalledWith(
+    "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/6.2.0.85879/sonar-scanner-6.2.0.85879-net.zip",
+  );
+  expect(toolLib.extractZip).toHaveBeenCalledWith("path/to/zip-scanner");
+  expect(tl.setVariable).toHaveBeenCalledWith(
+    TaskVariables.SonarQubeScannerLocation,
+    "path/to/unzip-scanner",
+  );
 });
