771cdd504059081eb3ad765fac79e6ed4b76e9b3
7PH <b.raymond@protonmail.com>
2024-05-29 13:44:35 +0200

    VSTS-376 Bump minimum agent version in SQ V6 & SC V2 tasks to ensure Node16 is available

--- src/common/latest/esbuild.config.js
+++ src/common/latest/esbuild.config.js
@@ -2,7 +2,7 @@ module.exports = {
   bundle: true,
   platform: "node",
   format: "cjs",
-  target: "node10",
+  target: "node16",
   minify: true,
   /**
    * @see https://github.com/microsoft/azure-pipelines-task-lib/issues/942#issuecomment-1904939900
