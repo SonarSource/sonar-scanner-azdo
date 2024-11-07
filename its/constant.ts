8c89464ad51669a879539c760b16caa2f600cf39
7PH <benjamin.raymond@sonarsource.com>
2024-11-07 11:13:36 +0100

    SONARAZDO-416 Auto-register QA pipelines to Azure

--- its/constant.ts
+++ its/constant.ts
@@ -2,6 +2,8 @@
 export const AZDO_BASE_URL = "https://dev.azure.com/";
 export const AZDO_ORGANIZATION = "sonarsource";
 export const AZDO_PROJECT = "SonarScannerAzdo";
+export const AZDO_PIPELINE_NAME_PREFIX = "pipeline-";
+export const AZDO_MAIN_PIPELINE_NAME = "SonarSource.sonar-scanner-azdo";
 
 // Information to bind to the SonarCloud used in the E2E tests
 export const SONARCLOUD_ORGANIZATION_KEY = "sonarsource-sonar-scanner-azdo-it";
@@ -16,27 +18,30 @@ export const DOTNET_VERSION_INPUT_NAME = "msBuildVersion";
 // All the *_KEY values are projects that should exist in SonarCloud's ${SONARCLOUD_ORGANIZATION_KEY} organization
 // All the *_PATH values are paths to the fixtures in the repository (relative to the root of the repository)
 
+export const FIXTURES_PATH = "its/fixtures";
+
 // CLI Project
 export const DUMMY_PROJECT_CLI_KEY = "sonarsource-sonar-scanner-azdo-it_dummy-project-cli";
-export const DUMMY_PROJECT_CLI_PATH = "its/fixtures/dummy-project-cli";
+export const DUMMY_PROJECT_CLI_PATH = FIXTURES_PATH + "/dummy-project-cli";
 
 // .NET Core Project
 export const DUMMY_PROJECT_DOTNET_CORE_KEY =
   "sonarsource-sonar-scanner-azdo-it_dummy-project-dotnet-core";
-export const DUMMY_PROJECT_DOTNET_CORE_PATH = "its/fixtures/dummy-project-dotnet-core";
+export const DUMMY_PROJECT_DOTNET_CORE_PATH = FIXTURES_PATH + "/dummy-project-dotnet-core";
 
 // .NET Framework Project
 export const DUMMY_PROJECT_DOTNET_FRAMEWORK_KEY =
   "sonarsource-sonar-scanner-azdo-it_dummy-project-dotnet-framework";
-export const DUMMY_PROJECT_DOTNET_FRAMEWORK_PATH = "its/fixtures/dummy-project-dotnet-framework";
+export const DUMMY_PROJECT_DOTNET_FRAMEWORK_PATH =
+  FIXTURES_PATH + "/dummy-project-dotnet-framework";
 
 // Gradle Project
 export const DUMMY_PROJECT_GRADLE_KEY = "sonarsource-sonar-scanner-azdo-it_dummy-project-gradle";
-export const DUMMY_PROJECT_GRADLE_PATH = "its/fixtures/dummy-project-gradle";
+export const DUMMY_PROJECT_GRADLE_PATH = FIXTURES_PATH + "/dummy-project-gradle";
 
 // Maven Project
 export const DUMMY_PROJECT_MAVEN_KEY = "sonarsource-sonar-scanner-azdo-it_dummy-project-maven";
-export const DUMMY_PROJECT_MAVEN_PATH = "its/fixtures/dummy-project-maven";
+export const DUMMY_PROJECT_MAVEN_PATH = FIXTURES_PATH + "/dummy-project-maven";
 
 // All tasks are suffixed with this string to avoid task-name conflicts with the real/dogfood extension
 export const TASK_NAME_PREFIX = "Test";
--- its/it/azdo.ts
+++ its/it/azdo.ts
@@ -1,12 +1,65 @@
 import * as vm from "azure-devops-node-api";
-import { Build, BuildResult, BuildStatus } from "azure-devops-node-api/interfaces/BuildInterfaces";
-import { AZDO_BASE_URL, AZDO_ORGANIZATION, AZDO_PROJECT } from "../constant";
+import { IBuildApi } from "azure-devops-node-api/BuildApi";
+import {
+  Build,
+  BuildDefinition,
+  BuildResult,
+  BuildStatus,
+} from "azure-devops-node-api/interfaces/BuildInterfaces";
+import path from "path";
+import {
+  AZDO_BASE_URL,
+  AZDO_MAIN_PIPELINE_NAME,
+  AZDO_ORGANIZATION,
+  AZDO_PIPELINE_NAME_PREFIX,
+  AZDO_PROJECT,
+  FIXTURES_PATH,
+} from "../constant";
 import { getBranch, loadEnvironmentVariables } from "./env";
 
 export function getAzdoApi(): vm.WebApi {
   const env = loadEnvironmentVariables();
   const credentialHandler = vm.getPersonalAccessTokenHandler(env.AZURE_TOKEN);
   return new vm.WebApi(AZDO_BASE_URL + AZDO_ORGANIZATION, credentialHandler);
+}
+
+export async function getTemplatePipeline(buildApi: IBuildApi): Promise<BuildDefinition> {
+  // Find an existing pipeline to use as a template
+  const definitions = await buildApi.getDefinitions(AZDO_PROJECT, AZDO_MAIN_PIPELINE_NAME);
+  const definition = definitions[0];
+  if (!definition) {
+    throw new Error(`The main pipeline ${AZDO_MAIN_PIPELINE_NAME} was not found. It's needed to be used as a template to create the other pipelines.`);
+  }
+  // Get full object
+  return buildApi.getDefinition(
+    AZDO_PROJECT,
+    definition.id as number,
+  );
+}
+
+export async function createPipeline(
+  buildApi: IBuildApi,
+  template: BuildDefinition,
+  pipelineName: string,
+): Promise<void> {
+  const existingPipelines = await buildApi.getDefinitions(AZDO_PROJECT, pipelineName);
+  if (existingPipelines.length > 0) {
+    console.log(`Pipeline ${pipelineName} already exists. Skipping creation.`);
+    return;
+  }
+
+  console.log(`Creating pipeline ${pipelineName}`);
+  await buildApi.createDefinition(
+    {
+      ...template,
+      process: {
+        ...template.process,
+        yamlFilename: path.join(FIXTURES_PATH, pipelineName + ".yml"),
+      } as unknown as any,
+      name: pipelineName,
+    },
+    AZDO_PROJECT,
+  );
 }
 
 export async function runPipeline(
