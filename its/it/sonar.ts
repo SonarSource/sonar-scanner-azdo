dec53499ee454c46e4c01ac36b8814d9b3ec9466
Julien HENRY <julien.henry@sonarsource.com>
2025-04-03 12:30:23 +0200

    Log errors

--- its/it/sonar.ts
+++ its/it/sonar.ts
@@ -43,6 +43,22 @@ export async function provisionProject(
       headers: {
         Authorization: `Bearer ${env.SONARCLOUD_TOKEN}`,
       },
+    }).catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
     });
     return true;
   } catch (error: unknown) {
