f55730dba00a0a271a7a5d3477a99c7c68361ca8
Julien HENRY <julien.henry@sonarsource.com>
2025-04-09 16:24:20 +0200

    Fix coverage reports

--- src/common/sonarqube-v5/jest.config.js
+++ src/common/sonarqube-v5/jest.config.js
@@ -1,8 +1,13 @@
+const path = require("path");
+
 module.exports = {
   clearMocks: true,
   collectCoverageFrom: ["**/*.ts", "!<rootDir>/node_modules/"],
   coverageDirectory: "coverage",
-  coverageReporters: ["lcovonly", "text"],
+  coverageReporters: [
+    ["lcovonly", { projectRoot: path.join(__dirname, "..", "..", "..") }],
+    "text",
+  ],
   moduleFileExtensions: ["ts", "js"],
   testPathIgnorePatterns: ["<rootDir>/node_modules", "<rootDir>/config", "<rootDir>/build"],
   testRegex: "(/__tests__/.*|\\-test)\\.ts$",
