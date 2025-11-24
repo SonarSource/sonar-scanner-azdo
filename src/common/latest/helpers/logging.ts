418a7d52cd3996fece29dc6ec2d258af0bd95256
Aurélien Poscia <aurelien.poscia@sonarsource.com>
2025-11-24 08:12:19 +0100

    SC-35440 scripted update of SonarSource SA to SonarSource Sàrl (#500)
    Co-authored-by: Samir M <samir.marini@sonarsource.com>

--- src/common/latest/helpers/logging.ts
+++ src/common/latest/helpers/logging.ts
@@ -1,6 +1,6 @@
 /*
  * sonar-scanner-npm
- * Copyright (C) 2022-2024 SonarSource SA
+ * Copyright (C) 2022-2024 SonarSource Sàrl
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
