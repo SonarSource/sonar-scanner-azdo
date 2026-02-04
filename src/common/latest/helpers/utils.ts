f119bc5b51ae11f8cce319e638fbd3c149a68c92
antoine.vinot <antoine.vinot@sonarsource.com>
2026-02-04 13:26:04 +0100

    SONARAZDO-536 Add detailed logs for Axios calls

--- src/common/latest/helpers/utils.ts
+++ src/common/latest/helpers/utils.ts
@@ -23,3 +23,25 @@ export function sanitizeScannerParams(scannerParams: ScannerParams) {
 export function isWindows() {
   return tl.getPlatform() === tl.Platform.Windows;
 }
+
+export function isDebug() {
+  return tl.getVariable("system.debug") === "true";
+}
+
+export function safeStringify(obj: unknown, space?: number): string {
+  const seen = new WeakSet();
+
+  return JSON.stringify(
+    obj,
+    (key, value) => {
+      if (typeof value === "object" && value !== null) {
+        if (seen.has(value)) {
+          return "[Circular]";
+        }
+        seen.add(value);
+      }
+      return value;
+    },
+    space,
+  );
+}
