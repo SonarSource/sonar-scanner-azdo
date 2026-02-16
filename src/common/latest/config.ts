9d47c712a7c978b8137c7b07a41395ac7d683c87
antoine.vinot <antoine.vinot@sonarsource.com>
2026-02-16 10:37:27 +0100

    SONARAZDO-534 Bump Scanner for .NET 11.1.0.132901

--- src/common/latest/config.ts
+++ src/common/latest/config.ts
@@ -1,5 +1,5 @@
 // When the user does not specify a specific version, these will be the default versions used.
-const dotnetScannerVersion = "11.0.0.126294";
+const dotnetScannerVersion = "11.1.0.132901";
 const cliScannerVersion = "8.0.1.6346";
 
 // MSBUILD scanner location
